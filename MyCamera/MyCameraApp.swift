3bb4be52664fab0a131b7c3b6763d6bbe57ebb90
yanagyi <yanagyi429@gmail.com>
2024-07-14 01:41:24 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Hiro Yanagisawa on 2024/07/14.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
